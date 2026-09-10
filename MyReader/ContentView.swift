1da3226d1562fa8d9a0bb088061cf8544563b4e5
quan yang <qyang@bupt.edu.cn>
2026-09-10 11:11:15 +0800

    Initial Commit

---
+++ MyReader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyReader
+//
+//  Created by 杨权 on 2026/9/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyReader/MyReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyReaderApp.swift
+//  MyReader
+//
+//  Created by 杨权 on 2026/9/10.
+//
+
+import SwiftUI
+
+@main
+struct MyReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
